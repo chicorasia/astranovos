d88a4c4996758611bc9a55a26e64e9c385cbae9a
chicorasia <chicorasia@gmail.com>
2021-11-10 08:57:49 -0300

    Adicionadas classes de modelo: Post e Launch

---
+++ app/src/main/java/br/com/chicorialabs/astranovos/data/model/Post.kt
@@ -0,0 +1,21 @@
+package br.com.chicorialabs.astranovos.data.model
+
+/**
+ * Essa data class representa uma publicação; os artigos, postagens
+ * em blog e relatórios possuem a mesma estrutura, então deve
+ * funcionar em qualquer um dos casos.
+ * A função hasLaunch() é um método de conveniência para
+ * indicar se há lançamentos associados à notícia.
+ */
+data class Post(
+    val id: Int,
+    val title: String,
+    val url: String,
+    val summary: String,
+    val publishedAt: String,
+    val updatedAt: String?,
+    val launches: Array<Launch> = emptyArray(),
+) {
+    fun hasLaunch(): Boolean = launches.isEmpty()
+
+}
